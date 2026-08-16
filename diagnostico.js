3874e73c8baa1d1d1bab573532ae8bb5c55fcef3
Tiago <tiagodelfini10@gmail.com>
2026-08-16 20:24:47 -0300

    Node

---
+++ diagnostico.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("Plataforma: " , process.platform);
+console.log("Versión de Node.js: " , process.version);
+console.log("Argumentos: " , process.argv);
+console.log("Directorio de trabajo: " , process.env);
